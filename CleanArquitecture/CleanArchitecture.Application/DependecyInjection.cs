61e48891589724589573e177cf34ee8768c78302
LG <lucianogaitan@trasusargentina.com>
2024-04-21 07:32:20 -0300

    add application cape and handler

---
+++ CleanArquitecture/CleanArchitecture.Application/DependecyInjection.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Domain.Rentals;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Application
+{
+	public static  class DependecyInjection
+	{
+		public static IServiceCollection AddApplication(this IServiceCollection services)
+		{
+			services.AddMediatR(configuration =>
+			{
+				configuration.RegisterServicesFromAssemblies(typeof(DependecyInjection).Assembly);
+			});
+
+			services.AddTransient<ServicePrice>();
+			return services;
+		}
+	}
+}
